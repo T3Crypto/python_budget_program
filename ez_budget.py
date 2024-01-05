de8c859761578cd47586f386a0294db13d44d9c3
T3Crypto <107518645+T3Crypto@users.noreply.github.com>
2024-01-05 02:58:48 +0100

    Add files via upload

--- ez_budget.py
+++ ez_budget.py
@@ -1,14 +1,43 @@
+from ez_calculations import hcalc
+from ez_calculations import fcalc
+from ez_calculations import tcalc
+
 deposit = float(input("Deposit: "))  # Deposit input prompt
 
-housing = round(0.25 * deposit, 2)
-food = round(0.1 * deposit, 2)
-transportation = round(0.15 * deposit, 2)
+print("Housing: " + str(round(deposit * hcalc, 2)))
+print("Food: " + str(round(deposit * fcalc, 2)))
+print("Transportation: " + str(round(deposit * tcalc, 2)))
 
-# housing = "{:.0%}".format(housing)
-housingresult = "Housing: " + str(housing)
-foodresult = "Food: " + str(food)
-transportationresult = "Transportation: " + str(transportation)
+# Version (1)
+# deposit = float(input("Deposit: "))  # Deposit input prompt
 
-print(housingresult)
-print(foodresult)
-print(transportationresult)
+# housing = float(input("Percentage for housing: "))
+# food = float(input("Percentage for food: "))
+# transportation = float(input("Percentage for transportation: "))
+
+# hcalc = deposit * (housing / 100)
+# fcalc = deposit * (food / 100)
+# tcalc = deposit * (transportation / 100)
+
+# hround = round(hcalc, 2)
+# fround = round(fcalc, 2)
+# tround = round(tcalc, 2)
+
+# hresult = "Housing: " + str(hround)
+# fresult = "Food: " + str(fround)
+# tresult = "Transportation: " + str(tround)
+
+# print(hresult)
+# print(fresult)
+# print(tresult)
+
+# Version (2)
+# deposit = float(input("Deposit: "))  # Entering Deposit amount
+
+# housing = float(input("Percentage of housing: ")) / 100.0
+# food = float(input("Percentage of food: ")) / 100.0
+# transportation = float(input("Percentage of transportation: ")) / 100.0
+
+# print("Housing: " + str(round(deposit * housing, 2)))
+# print("Food: " + str(round(deposit * food, 2)))
+# print("Transportation: " + str(round(deposit * transportation, 2)))
